98ceafe1185430290362afe80c9c7441118a8c24
kazuya <ketsume0211@gmail.com>
2016-04-02 13:19:02 +0900

    add python tips

--- setup.sh
+++ setup.sh
@@ -13,7 +13,7 @@ apt-get install -y fonts-takao fcitx fcitx-mozc language-pack-ja im-config fonts
 im-config -n fcitx
 
 # Development tools
-apt-get install -y vim net-tools bc gcc g++ make chromium-browser git screen ssh emacs python-dev
+apt-get install -y vim net-tools bc gcc g++ make chromium-browser git screen ssh emacs 
 
 # Maintenance tools
 apt-get install -y gparted 
@@ -30,6 +30,10 @@ apt-get install -y sqlite3 libsqlite3-dev # SQLite
 apt-get install -y imagemagick # MiniMagick RMagick
 apt-get install -y libxslt-dev libxml2-dev # Nokogiri(kaminari...)
 apt-get install -y build-essential git-core curl libreadline6 libreadline6-dev libmysqlclient-dev libffi-dev nodejs
+
+# for python
+apt-get install -y python-dev python-pip python3 python3-pip gfortran liblapack-dev
+pip install virtualenv
 
 # For android 
 apt-get install -y lib32z1 lib32ncurses5 lib32bz2-1.0 lib32stdc++6
