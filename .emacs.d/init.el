3d71e5066d1547ee44d231720150800a16b1d665
gosho-kazuya <ketsume0211@gmail.com>
2016-04-30 13:45:12 +0900

    Change IME ON\OFF key

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,11 +1,33 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Package.el
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(require 'package)
+
+;; MELPA
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+;; MELPA-stable
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+;; Marmalade
+(add-to-list 'package-archives  '("marmalade" . "http://marmalade-repo.org/packages/") t)
+;; Org
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+
+(package-initialize)
+
 ; Add path
 (add-to-list 'load-path "~/.emacs.d/lisp")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Markdown
+;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ; Qiita markdown
 (require 'ox-qmd)
 (autoload 'markdown-mode "markdown-mode.el" "Major mode for editing Markdown files" t)
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 
+; Markdown Table
 (defun cleanup-org-tables ()
   (save-excursion
     (goto-char (point-min))
@@ -14,29 +36,6 @@
 (add-hook 'markdown-mode-hook
           #'(lambda()
           (add-hook 'after-save-hook 'cleanup-org-tables  nil 'make-it-local)))
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Japanese input using Mozc
-;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(require 'mozc)
-(set-language-environment "Japanese")
-(setq default-input-method "japanese-mozc")
-(setq mozc-candidate-style 'overlay)
-
-;; Change IME ON/OFF key
-(bind-key* "C-i"
-    (lambda()
-        (interactive)
-	(if current-input-method (inactivate-input-method))
-	    (toggle-input-method)))
-(bind-key* (kbd "C-u")
-    (lambda()
-        (interactive)
-	(inactivate-input-method)))
-
-
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; File
@@ -53,15 +52,13 @@
 ;; Edit
 ;;;;;;;;;;;;;;;;;;;;;;;;;;
 (setq kill-whole-line t)
+(require 'auto-complete)
+(global-auto-complete-mode t)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Key settings
 ;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; Change C-h -> BackSpace
-(keyboard-translate ?\C-h ?\C-?)
-(global-set-key "\C-h" nil)
-(global-set-key "\C-ch" 'help-command)
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -75,28 +72,39 @@
 (set-face-background 'show-paren-match-face "grey")
 (set-face-foreground 'show-paren-match-face "black")
 
-;; Package.el
 
-(require 'package)
-
-;; MELPA
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
-;; MELPA-stable
-(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
-;; Marmalade
-(add-to-list 'package-archives  '("marmalade" . "http://marmalade-repo.org/packages/") t)
-;; Org
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
-
-(package-initialize)
-
-
+;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; Set UTF-8 to default
+;;;;;;;;;;;;;;;;;;;;;;;;;;
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
 (set-buffer-file-coding-system 'utf-8)
 (setq default-buffer-file-coding-system 'utf-8)
 (set-default-coding-systems 'utf-8)
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Japanese input using Mozc
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(require 'mozc)
+(set-language-environment "Japanese")
+(setq default-input-method "japanese-mozc")
+(setq mozc-candidate-style 'overlay)
+
+
+;; Change IME ON/OFF key
+(require 'bind-key)
+(bind-key* "C-j"
+    (lambda()
+        (interactive)
+        (if current-input-method (inactivate-input-method))
+            (toggle-input-method)))
+(bind-key* "C-h"
+    (lambda()
+        (interactive)
+        (inactivate-input-method)))
 
 
 
