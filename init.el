283f4dbfb31b82e067f9b835f80fe3dc68fa6f72
gosho-kazuya <ketsume0211@gmail.com>
2016-04-30 12:18:45 +0900

    Add markdown-mode

--- init.el
+++ init.el
@@ -3,6 +3,8 @@
 
 ; Qiita markdown
 (require 'ox-qmd)
+(autoload 'markdown-mode "markdown-mode.el" "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -15,12 +17,12 @@
 (setq mozc-candidate-style 'overlay)
 
 ;; Change C-j -> C-]
-(global-set-key (kbd "C-j")
+(global-set-key (kbd "C-i")
     (lambda()
         (interactive)
 	(if current-input-method (inactivate-input-method))
 	    (toggle-input-method)))
-(global-set-key (kbd "M-j")
+(global-set-key (kbd "C-u")
     (lambda()
         (interactive)
 	(inactivate-input-method)))
@@ -56,10 +58,25 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;
 (line-number-mode 1)
 
-;; Hilight () 
+;; Hilight ()
 (show-paren-mode 1)
 (setq show-paren-style 'mixed)
 (set-face-background 'show-paren-match-face "grey")
 (set-face-foreground 'show-paren-match-face "black")
+
+;; Package.el
+
+(require 'package)
+
+;; MELPA
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+;; MELPA-stable
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+;; Marmalade
+(add-to-list 'package-archives  '("marmalade" . "http://marmalade-repo.org/packages/") t)
+;; Org
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+
+(package-initialize)
 
 
