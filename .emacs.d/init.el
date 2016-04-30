461bed5529931b873df8f659f66d4f40baa5c7ed
gosho-kazuya <ketsume0211@gmail.com>
2016-04-30 12:30:55 +0900

    Delete emacs caches

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -6,15 +6,6 @@
 (autoload 'markdown-mode "markdown-mode.el" "Major mode for editing Markdown files" t)
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 
-(defun cleanup-org-tables ()
-  (save-excursion
-    (goto-char (point-min))
-    (while (search-forward "-+-" nil t) (replace-match "-|-"))))
-(add-hook 'markdown-mode-hook 'orgtbl-mode)
-(add-hook 'markdown-mode-hook
-          #'(lambda()
-          (add-hook 'after-save-hook 'cleanup-org-tables  nil 'make-it-local)))
-
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Japanese input using Mozc
