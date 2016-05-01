72ad75700c0a71fb94d558fcb3aabfe6f1af5fbf
gosho-kazuya <ketsume0211@gmail.com>
2016-05-01 23:59:07 +0900

    Use sudo

--- setup.sh
+++ setup.sh
@@ -1,62 +1,44 @@
 #!/bin/bash
 
-MY_HOME=/home/YOUR_HOME
-
 # Move dotfile to home folder
-cp -rf .* $MY_HOME
+cp -rf .config .emacs.d .vimrc ~
 
-# Home folder
-cd $MY_HOME || exit
+# Move home folder
+cd
 
 # General update
-apt-get update
-apt-get upgrade
+sudo apt-get update
+sudo apt-get upgrade
 
-# Remove unuse items
-apt-get remove --purge -y netsurf*
-rm -rf .netsurf
+# Remove unnecessary items
+sudo apt-get remove --purge -y netsurf*
+rm -rf ~/.netsurf
 
 # Language and inputs
-apt-get install -y fonts-takao fcitx fcitx-mozc language-pack-ja im-config fonts-wqy-zenhei emacs-mozc
+sudo apt-get install -y fonts-takao fcitx fcitx-mozc language-pack-ja im-config fonts-wqy-zenhei emacs-mozc
 im-config -n fcitx
 
 # Development tools
-apt-get install -y vim net-tools bc gcc g++ make chromium-browser git screen ssh emacs tree colordiff
+sudo apt-get install -y vim net-tools bc gcc g++ make chromium-browser git screen ssh emacs tree colordiff
 
 # Maintenance tools
-apt-get install -y gparted
+sudo apt-get install -y gparted man
 
 # UX utilities
-apt-get install -y touchegg
+sudo apt-get install -y touchegg
 
 # Multimedia
-apt-get install -y vlc ubuntu-restricted-extras
+sudo apt-get install -y vlc ubuntu-restricted-extras
 
 # For ruby installation
-apt-get install -y libreadline-dev libssl-dev libsqlite3-dev zlib1g-dev libyaml-dev libssl-dev libsqlite3-dev
-apt-get install -y sqlite3 libsqlite3-dev # SQLite
-apt-get install -y imagemagick # MiniMagick RMagick
-apt-get install -y libxslt-dev libxml2-dev # Nokogiri(kaminari...)
-apt-get install -y build-essential git-core curl libreadline6 libreadline6-dev libmysqlclient-dev libffi-dev nodejs
-
-# For python
-apt-get install -y python-dev python-pip python3 python3-pip gfortran liblapack-dev libpng12-dev libfreetype6-dev
-pip install virtualenv
-pip install jupyter
-
-# For android
-apt-get install -y lib32z1 lib32ncurses5 lib32bz2-1.0 lib32stdc++6
-
-# For Elasticsearch
-apt-get install -y openjdk-7-jdk
-
-exit
-
-# Additional software follow.
-# If you want to install them, remove 'exit' above.
+sudo apt-get install -y libreadline-dev libssl-dev libsqlite3-dev zlib1g-dev libyaml-dev libssl-dev libsqlite3-dev
+sudo apt-get install -y sqlite3 libsqlite3-dev
+sudo apt-get install -y imagemagick # MiniMagick RMagick
+sudo apt-get install -y libxslt-dev libxml2-dev # Nokogiri
+sudo apt-get install -y build-essential git-core curl libreadline6 libreadline6-dev libmysqlclient-dev libffi-dev nodejs
 
 # Install ruby using rbenv + Ruby-build
-git clone https://github.com/sstephenson/rbenv.git .rbenv
+git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> .bash_profile
 echo 'eval "$(rbenv init -)"' >> .bash_profile
 exec $SHELL -l
@@ -65,6 +47,17 @@ rbenv install -v 2.2.0
 rbenv rehash
 rbenv global 2.2.0
 
+# For python
+sudo apt-get install -y python-dev python-pip python3 python3-pip gfortran liblapack-dev libpng12-dev libfreetype6-dev
+pip install virtualenv
+pip install jupyter
+
+# For android
+sudo apt-get install -y lib32z1 lib32ncurses5 lib32bz2-1.0 lib32stdc++6
+
+# For Elasticsearch
+sudo apt-get install -y openjdk-7-jdk
+
 # Change runlevel via Systemd
-systemctl set-default multi-user.target
+sudo systemctl set-default multi-user.target
 
